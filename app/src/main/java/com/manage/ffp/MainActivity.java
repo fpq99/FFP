943207b30bcc87309add0628f5969b0335327979
fpq99 <fpq00@naver.com>
2017-12-28 17:47:31 +0900

    노티 내용 받아오기

--- app/src/main/java/com/manage/ffp/MainActivity.java
+++ app/src/main/java/com/manage/ffp/MainActivity.java
@@ -1,5 +1,9 @@
 package com.manage.ffp;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -18,12 +22,28 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        startActivity(new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS"));
+
+        //노티 생성
+        final NotificationManager notificationManager= (NotificationManager)MainActivity.this.getSystemService(MainActivity.this.NOTIFICATION_SERVICE);
+        final Intent intent = new Intent(MainActivity.this.getApplicationContext(),MainActivity.class);
+
+        final Notification.Builder builder = new Notification.Builder(getApplicationContext());
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+                PendingIntent pendnoti = PendingIntent.getActivity(MainActivity.this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                builder.setSmallIcon(R.drawable.ic_launcher_background).setTicker("HIMAN").setWhen(System.currentTimeMillis())
+                        .setNumber(1).setContentTitle("test noti").setContentText("noti content")
+                        .setDefaults(Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE).setContentIntent(pendnoti).setAutoCancel(true).setOngoing(true);
+                notificationManager.notify(1, builder.build());
             }
         });
     }
--- app/src/main/java/com/manage/ffp/NotificationCatch.java
+++ app/src/main/java/com/manage/ffp/NotificationCatch.java
@@ -1,8 +1,15 @@
 package com.manage.ffp;
 
+import android.app.Notification;
+import android.os.Bundle;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
+import android.util.Log;
+import android.widget.RemoteViews;
 import android.widget.Toast;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 
 public class NotificationCatch extends NotificationListenerService {
 
@@ -10,11 +17,68 @@ public class NotificationCatch extends NotificationListenerService {
     public void onNotificationPosted(StatusBarNotification sbn) {
         super.onNotificationPosted(sbn);
         String pack = sbn.getPackageName();
-        Toast.makeText(getApplicationContext(), pack+"\n"+sbn.toString(), Toast.LENGTH_SHORT).show();
+        String text = sbn.getNotification().tickerText.toString()+", "+sbn.getNotification().extras.size();
+
+        Bundle bundle =  sbn.getNotification().extras;
+        for(String str : bundle.keySet()) {
+            if(bundle.get(str) != null) {
+                Log.i("noti bundle", "Extra KEY: "+str+" :: "+bundle.get(str).toString());
+            }
+        }
+
+        Log.d("NOTI LOGG", pack+"\n"+text);
+        
     }
 
     @Override
     public void onNotificationRemoved(StatusBarNotification sbn) {
         super.onNotificationRemoved(sbn);
+    }
+
+
+    private String getText(StatusBarNotification event)
+    {
+        String result = null;
+        try
+        {
+            Notification notification = event.getNotification();
+            RemoteViews views = notification.contentView;
+            Class<?> secretClass = views.getClass();
+
+            Field outerFields[] = secretClass.getDeclaredFields();
+            for (int i = 0; i < outerFields.length; i++)
+            {
+                if (!outerFields[i].getName().equals("mActions"))
+                    continue;
+
+                outerFields[i].setAccessible(true);
+
+                @SuppressWarnings("unchecked")
+                ArrayList<Object> actions = (ArrayList<Object>) outerFields[i].get(views);
+                for (Object action : actions)
+                {
+                    Field innerFields[] = action.getClass().getDeclaredFields();
+
+                    Object value = null;
+                    Integer type = null;
+                    for (Field field : innerFields)
+                    {
+                        field.setAccessible(true);
+                        if (field.getName().equals("value"))
+                            value = field.get(action);
+                        else if (field.getName().equals("type"))
+                            type = field.getInt(action);
+                    }
+
+                    if (type != null && type == 10)
+                        result = value.toString();
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
